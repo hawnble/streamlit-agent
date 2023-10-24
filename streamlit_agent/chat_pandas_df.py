a8e3e695da169e20c8b0f1547d8fd7934240171e
hawnble <135021067+hawnble@users.noreply.github.com>
2023-10-24 20:40:45 +0900

    Update chat_pandas_df.py

--- streamlit_agent/chat_pandas_df.py
+++ streamlit_agent/chat_pandas_df.py
@@ -143,14 +143,15 @@ df_sorted = df_filtered.groupby('Manufacturer').apply(lambda x: x.nlargest(1, 'V
 # # 파일이 업로드된 경우 데이터를 로드합니다.
 # if uploaded_file:
 #     df = load_data(uploaded_file)
-
+intro = '''저는 노트북을 전문적으로 추천해주는 챗봇 Pick-Chat 이에요!
+질문을 상세히 작성해 주시면 정확한 답변이 가능해요!'''
 # OpenAI API 키 입력을 받습니다.
 #openai_api_key = st.sidebar.text_input("OpenAI API Key", type="password")
 openai_api_key = st.secrets["openai_key"]
 # 대화 기록을 초기화하거나 버튼을 눌러 대화 기록을 삭제합니다.
 if "messages" not in st.session_state or st.button("Clear conversation history"):# 
     # 초기 대화 메시지 설정
-    st.session_state["messages"] = [{"role": "assistant", "content": '저는 노트북을 전문적으로 추천해주는 챗봇 Pick-Chat 이에요!'\n'질문을 상세히 작성해 주시면 정확한 답변이 가능해요!'}]
+    st.session_state["messages"] = [{"role": "assistant", "content": intro }]
 
 # 이전 대화 내용을 표시합니다.
 for msg in st.session_state.messages:
