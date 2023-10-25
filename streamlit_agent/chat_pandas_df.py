996d80a2c0f84386c0e33e9a7f255cf83baa56a8
hawnble <135021067+hawnble@users.noreply.github.com>
2023-10-25 13:27:09 +0900

    Update chat_pandas_df.py

--- streamlit_agent/chat_pandas_df.py
+++ streamlit_agent/chat_pandas_df.py
@@ -65,9 +65,7 @@ st.image(im_logo)
 st.markdown("""
 <style>
 .st-emotion-cache-18ni7ap {
-    #left: 1100px;
-    #right: -1000px;
-    #display: none;
+
     visibility: hidden;
 }
 .st-emotion-cache-1pxazr7 {
