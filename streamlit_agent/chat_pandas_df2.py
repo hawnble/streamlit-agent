089360f29343a73f1260be577a61b16b774bd57c
hawnble <135021067+hawnble@users.noreply.github.com>
2023-10-26 01:06:57 +0900

    Update chat_pandas_df2.py

--- streamlit_agent/chat_pandas_df2.py
+++ streamlit_agent/chat_pandas_df2.py
@@ -65,8 +65,8 @@ st.image(im_logo)
 hide_streamlit_style = """
             <style>
             #MainMenu {visibility: hidden;}
-            #footer {visibility: hidden;}
-            #header {visibility: hidden;}
+            footer {visibility: hidden;}
+            header {visibility: hidden;}
             #.st-emotion-cache-18ni7ap {visibility: hidden;}
             .st-emotion-cache-1pxazr7 {visibility: hidden;}
             .st-emotion-cache-eczf16 {visibility: hidden;}
